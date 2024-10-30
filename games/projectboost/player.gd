c4b7ea7b8c0b9a22771818d239b6047cc4278a38
David030822 <demeter.david0322@gmail.com>
2024-10-30 23:08:15 +0200

    The big return - adding some Tweens

--- games/projectboost/player.gd
+++ games/projectboost/player.gd
@@ -6,6 +6,8 @@ extends RigidBody3D
 ## How fast to rotate left or right.
 @export var torque_thrust: float = 100.0
 
+var is_transitioning: bool = false
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
@@ -18,16 +20,27 @@ func _process(delta: float) -> void:
 		apply_torque(Vector3(0.0, 0.0, -torque_thrust * delta))
 
 func _on_body_entered(body: Node) -> void:
-	if "Goal" in body.get_groups():
-		complete_level()
-		
-	if "Hazard" in body.get_groups():
-		crash_sequence()
+	if is_transitioning == false:
+		if "Goal" in body.get_groups():
+			complete_level(body.file_path)
+			
+		if "Hazard" in body.get_groups():
+			crash_sequence()
 		
 func crash_sequence() -> void:
 	print("KABOOM!")
-	get_tree().reload_current_scene()
+	set_process(false)
+	is_transitioning = true
+	var tween = create_tween()
+	tween.tween_interval(1.0)
+	tween.tween_callback(get_tree().reload_current_scene)
 	
-func complete_level() -> void:
+func complete_level(next_level_file: String) -> void:
 	print("Level Complete!")
-	get_tree().quit()
+	set_process(false)
+	is_transitioning = true
+	var tween = create_tween()
+	tween.tween_interval(1.0)
+	tween.tween_callback(
+		get_tree().change_scene_to_file.bind(next_level_file)
+	)
