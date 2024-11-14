221c0a7c3e7d50efa3b45dc24c66e8a5924b3fb7
David030822 <demeter.david0322@gmail.com>
2024-11-14 21:36:53 +0200

    Managing enemy spawning + scaling enemy health + adding money

--- games/barbarian-blaster/Enemy/enemy.gd
+++ games/barbarian-blaster/Enemy/enemy.gd
@@ -2,6 +2,7 @@ extends PathFollow3D
 
 @export var speed: float = 2.5
 @export var max_health := 50
+@export var enemy_reward := 15
 
 var current_health: int:
 	set(health_in):
@@ -10,12 +11,15 @@ var current_health: int:
 		current_health = health_in
 		if current_health < 1:
 			queue_free()
+			bank.gold += enemy_reward
 
 @onready var base = get_tree().get_first_node_in_group("base")
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var bank = get_tree().get_first_node_in_group("bank")
 
 func _ready() -> void:
 	current_health = max_health
+	Engine.time_scale = 2
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -23,3 +27,4 @@ func _process(delta: float) -> void:
 	if progress_ratio == 1.0:
 		base.take_damage()
 		set_process(false)
+		queue_free()
