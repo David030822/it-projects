454e45092628e399557fc5555eb473617b8a6da0
David030822 <demeter.david0322@gmail.com>
2024-11-21 20:36:48 +0200

    Adding enemy attack animation + weapon for the player

--- games/robo-rampage/robo-rampage/Enemy/enemy.gd
+++ games/robo-rampage/robo-rampage/Enemy/enemy.gd
@@ -1,6 +1,9 @@
 extends CharacterBody3D
 
+@export var attack_range := 1.5
+
 @onready var navigation_agent_3d: NavigationAgent3D = $NavigationAgent3D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 var player
 var provoked := false
@@ -28,7 +31,12 @@ func _physics_process(delta: float) -> void:
 	if distance <= aggro_range:
 		provoked = true
 		
+	if provoked:
+		if distance <= attack_range:
+			animation_player.play("Attack")
+		
 	if direction:
+		look_at_target(direction)
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
@@ -36,3 +44,11 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
+func look_at_target(direction: Vector3) -> void:
+	var adjusted_direction = direction
+	adjusted_direction.y = 0
+	look_at(global_position + adjusted_direction, Vector3.UP, true)
+
+func attack() -> void:
+	print("Enemy Attack!")
